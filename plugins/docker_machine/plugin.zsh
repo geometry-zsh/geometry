ed84d8b8e8e0a5a36f81cf7bb4f2809fbab1b9b0
keelan542 <keelanb542@gmail.com>
2016-12-14 23:14:14 +0000

    Deleted unneeded flags and emptied setup function

--- plugins/docker_machine/plugin.zsh
+++ plugins/docker_machine/plugin.zsh
@@ -4,11 +4,7 @@ GEOMETRY_COLOR_DOCKER_MACHINE=${GEOMETRY_COLOR_DOCKER_MACHINE:-blue}
 # Symbol definitions
 GEOMETRY_SYMBOL_DOCKER_MACHINE=${GEOMETRY_SYMBOL_DOCKER_MACHINE:-"⚓"}
 
-# Flags
-PROMPT_DOCKER_MACHINE=${PROMPT_DOCKER_MACHINE:-false}
-
 geometry_prompt_docker_machine_setup() {
-    return $PROMPT_DOCKER_MACHINE
 }
 
 geometry_prompt_docker_machine_render() {
