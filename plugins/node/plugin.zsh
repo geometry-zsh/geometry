faf23031798650fd75992f001e05c9d7f50ec74b
Okusanya Oluwadamilola <okusanya.damilola@gmail.com>
2017-01-11 00:58:47 +0000

    Created scaffolding file

---
+++ plugins/node/plugin.zsh
@@ -0,0 +1,10 @@
+# Color definitions
+GEOMETRY_COLOR_NODE=${GEOMETRY_COLOR_PROMPT:-blue}
+
+geometry_prompt_node_setup() {}
+
+geometry_prompy_virtualenv_render(){
+
+}
+
+geometry_plugin_register node
