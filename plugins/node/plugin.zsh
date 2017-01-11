edf448f392697d0345b04368347be2fa8d5d2ddb
Okusanya Oluwadamilola <okusanya.damilola@gmail.com>
2017-01-11 05:47:22 +0000

    Made the information print on the terminal

--- plugins/node/plugin.zsh
+++ plugins/node/plugin.zsh
@@ -1,10 +1,14 @@
-# Color definitions
-GEOMETRY_COLOR_NODE=${GEOMETRY_COLOR_PROMPT:-blue}
+#Symbol definitions
+GEOMETRY_VERSION_SYMBOL=${GEOMETRY_VERSION_SYMBOL:-"o"}
 
 geometry_prompt_node_setup() {}
 
-geometry_prompy_virtualenv_render(){
-
+geometry_prompt_node_render(){
+	local node_path="$echo "$(which node)" -v"
+	local npm_path="$echo "$(which npm)" --version"
+	GEOMETRY_NODE_VERSION=`(eval ${node_path})`
+	GEOMETRY_NPM_VERSION=`(eval ${npm_path})`
+	echo "$GEOMETRY_VERSION_SYMBOL $GEOMETRY_NODE_VERSION ($GEOMETRY_NPM_VERSION)"
 }
 
 geometry_plugin_register node
