29a9273b15c85bcf8f6a05f5481d54cebc3dd627
Okusanya Oluwadamilola <okusanya.damilola@gmail.com>
2017-01-12 00:16:41 +0000

    Prints the node and npm versions as given: node version (npm version)
    	* Bug: Can only print character symbols not the given symbol
    
    	* Bug: Prints on initial login.
    
    	* Enhancement: Print only after checking for yarn/nvm/nodeenv installation

--- plugins/node/plugin.zsh
+++ plugins/node/plugin.zsh
@@ -1,14 +1,18 @@
-#Symbol definitions
-GEOMETRY_VERSION_SYMBOL=${GEOMETRY_VERSION_SYMBOL:-"o"}
+# Color definitions
+GEOMETRY_COLOR_NODE_NPM_VERISON=${GEOMETRY_COLOR_NODE_NPM_VERISON:-black}
 
-geometry_prompt_node_setup() {}
+# Symbol definitions
+GEOMETRY_SYMBOL_NODE_NPM_VERISON=${GEOMETRY_SYMBOL_NODE_NPM_VERSION:-"⬡"}
+GEOMETRY_NODE_NPM_VERISON=$(prompt_geometry_colorize $GEOMETRY_COLOR_NODE_NPM_VERISON GEOMETRY_SYMBOL_NODE_NPM_VERISON) 
+
+geometry_prompt_node_setup() 
 
 geometry_prompt_node_render(){
 	local node_path="$echo "$(which node)" -v"
 	local npm_path="$echo "$(which npm)" --version"
 	GEOMETRY_NODE_VERSION=`(eval ${node_path})`
 	GEOMETRY_NPM_VERSION=`(eval ${npm_path})`
-	echo "$GEOMETRY_VERSION_SYMBOL $GEOMETRY_NODE_VERSION ($GEOMETRY_NPM_VERSION)"
+	echo "$GEOMETRY_NODE_NPM_VERSION $GEOMETRY_NODE_VERSION ($GEOMETRY_NPM_VERSION)"
 }
 
 geometry_plugin_register node
