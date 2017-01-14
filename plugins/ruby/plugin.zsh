fb20bb95096dbb4cc0e040e8160c4f7134deba53
reuben453 <reuben453@gmail.com>
2017-01-15 00:56:28 +0530

    * Cache ruby and rvm versions in the setup

--- plugins/ruby/plugin.zsh
+++ plugins/ruby/plugin.zsh
@@ -5,25 +5,39 @@ GEOMETRY_COLOR_RUBY_RVM_VERSION=${GEOMETRY_COLOR_PROMPT:-white}
 GEOMETRY_SYMBOL_RUBY_RVM_VERSION=${GEOMETRY_SYMBOL_RUBY_RVM_VERSION:-"◆"}
 GEOMETRY_RUBY_RVM_VERSION=$(prompt_geometry_colorize $GEOMETRY_COLOR_RUBY_RVM_VERSION $GEOMETRY_SYMBOL_RUBY_RVM_VERSION) 
 
-geometry_prompt_ruby_setup() {}
+get_full_ruby_version() {
+  if (( $+commands[ruby] )) && [[ -z $GEOMETRY_RUBY_VERSION_FULL ]]; then
+      GEOMETRY_RUBY_VERSION_FULL="$(ruby -v)"
+  fi
+}
 
-geometry_prompt_ruby_render(){
+get_full_rvm_version() {
+  if (( $+commands[rvm] )) && [[ -z $GEOMETRY_RVM_VERSION_FULL ]]; then
+      GEOMETRY_RVM_VERSION_FULL="$(rvm -v)"
+  fi
+}
+
+geometry_prompt_ruby_setup() {
+  GEOMETRY_RUBY_VERSION_FULL="$(ruby -v)"
+
+  GEOMETRY_RVM_VERSION_FULL="$(rvm -v)"
+}
+
+geometry_prompt_ruby_render() {
 
   if (( ! $+commands[ruby] )); then
       return "";
   fi
-  GEOMETRY_RUBY_VERSION_FULL="$(ruby -v)"
+  get_full_ruby_version
   [[ $GEOMETRY_RUBY_VERSION_FULL =~ 'ruby ([0-9a-zA-Z.]+)' ]]
   GEOMETRY_RUBY_VERSION=$match[1]
 
+  result="$GEOMETRY_RUBY_RVM_VERSION $GEOMETRY_RUBY_VERSION"
+
   if (( $+commands[rvm] )); then
-      GEOMETRY_RVM_VERSION_FULL="$(rvm -v)"
+      get_full_rvm_version
       [[ $GEOMETRY_RVM_VERSION_FULL =~ 'rvm ([0-9a-zA-Z.]+)'  ]]
       GEOMETRY_RVM_VERSION=$match[1]
-  fi
-
-  result="$GEOMETRY_RUBY_RVM_VERSION $GEOMETRY_RUBY_VERSION"
-  if [[ ! -z $GEOMETRY_RVM_VERSION ]]; then
       result=$result" ($GEOMETRY_RVM_VERSION)"
   fi
 
