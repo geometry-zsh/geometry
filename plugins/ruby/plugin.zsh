f7c3bca23da4e96f96655227afd7473bc636bd28
reuben453 <reuben@vacationlabs.com>
2017-01-17 11:52:57 +0530

    * Remove the check for existence of the GEOMETRY_RUBY_VERSION_FULL and GEOMETRY_RVM_VERSION_FULL vars before setting them * Keep the setup function empty * No need to cd to pwd before doing rvm current * Call geometry_prompt_get_full_ruby_version and geometry_prompt_get_full_rvm_version in render

--- plugins/ruby/plugin.zsh
+++ plugins/ruby/plugin.zsh
@@ -8,37 +8,31 @@ GEOMETRY_SYMBOL_RUBY_RVM_VERSION=${GEOMETRY_SYMBOL_RUBY_RVM_VERSION:-"◆"}
 GEOMETRY_RUBY_RVM_VERSION=$(prompt_geometry_colorize $GEOMETRY_COLOR_RUBY_RVM_VERSION $GEOMETRY_SYMBOL_RUBY_RVM_VERSION) 
 
 geometry_prompt_get_full_ruby_version() {
-  if (( $+commands[ruby] )) && [[ -z $GEOMETRY_RUBY_VERSION_FULL ]]; then
+  if (( $+commands[ruby] )); then
       GEOMETRY_RUBY_VERSION_FULL="$(ruby -v)"
   fi
 }
 
 geometry_prompt_get_full_rvm_version() {
-  if (( $+commands[rvm] )) && [[ -z $GEOMETRY_RVM_VERSION_FULL ]]; then
+  if (( $+commands[rvm] )); then
       GEOMETRY_RVM_VERSION_FULL="$(rvm -v)"
   fi
 }
 
-geometry_prompt_ruby_setup() {
-  geometry_prompt_get_full_ruby_version
-
-  geometry_prompt_get_full_rvm_version
-}
+geometry_prompt_ruby_setup() {}
 
 geometry_prompt_current_rvm_gemset_name() {
   if $GEOMETRY_RUBY_RVM_SHOW_GEMSET; then
       local cur_dir=$(pwd)
-      local gemset_name=$(cd "$(pwd)" && rvm current)
+      local gemset_name=$(rvm current)
       [[ $gemset_name =~ 'ruby-[0-9.]+@?(.*)' ]]
 
       # If no name present, then it's the default gemset
       if [[ -z $match[1] ]]; then
-          return "default"
+          echo "default"
       else
-          return $match[1]
+          echo $match[1]
       fi
-  else
-    return ""
   fi
 }
 
@@ -47,18 +41,20 @@ geometry_prompt_ruby_render() {
       return "";
   fi
 
+  geometry_prompt_get_full_ruby_version
   [[ $GEOMETRY_RUBY_VERSION_FULL =~ 'ruby ([0-9a-zA-Z.]+)' ]]
   GEOMETRY_RUBY_VERSION=$match[1]
 
   local result="$GEOMETRY_RUBY_RVM_VERSION $GEOMETRY_RUBY_VERSION"
 
   if (( $+commands[rvm] )); then
+      geometry_prompt_get_full_rvm_version
       [[ $GEOMETRY_RVM_VERSION_FULL =~ 'rvm ([0-9a-zA-Z.]+)'  ]]
       GEOMETRY_RVM_VERSION=$match[1]
       result=$result" ($GEOMETRY_RVM_VERSION"
 
       # Add current gemset name
-      local rvm_gemset_name=$geometry_prompt_current_rvm_gemset_name
+      local rvm_gemset_name=$( geometry_prompt_current_rvm_gemset_name )
       if [[ ! -z $rvm_gemset_name ]]; then
           result=$result" $rvm_gemset_name"
       fi
