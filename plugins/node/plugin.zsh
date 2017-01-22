ba6a2450088634cd34b5e686b297fa5faf850e55
Okusanya Oluwadamilola <okusanya.damilola@gmail.com>
2017-01-22 16:38:47 +0000

    Convert to tabs to spaces and remove temporary variable

--- plugins/node/plugin.zsh
+++ plugins/node/plugin.zsh
@@ -5,20 +5,17 @@ GEOMETRY_COLOR_NODE_NPM_VERSION=${GEOMETRY_COLOR_NODE_NPM_VERSION:-black}
 GEOMETRY_SYMBOL_NODE_NPM_VERSION=${GEOMETRY_SYMBOL_NODE_NPM_VERSION:-"⬡"}
 GEOMETRY_NODE_NPM_VERSION=$(prompt_geometry_colorize $GEOMETRY_COLOR_NODE_NPM_VERSION $GEOMETRY_SYMBOL_NODE_NPM_VERSION) 
 
-local node='node'
-local npm='npm'
 
 geometry_prompt_node_setup(){} 
 
 geometry_prompt_node_render(){
-	local n=$+commands[$node]
-	if (( $n == 0 )); then
-		echo ""
-	else
-		GEOMETRY_NODE_VERSION="$($node -v)"
-		GEOMETRY_NPM_VERSION="$($npm --version)" 
-		echo "$GEOMETRY_NODE_NPM_VERSION $GEOMETRY_NODE_VERSION ($GEOMETRY_NPM_VERSION)"
-	fi
+    if (( $+commands ['node'] == 0 )); then
+        echo ""
+    else
+        GEOMETRY_NODE_VERSION="$(node -v)"
+        GEOMETRY_NPM_VERSION="$(npm --version)" 
+        echo "$GEOMETRY_NODE_NPM_VERSION $GEOMETRY_NODE_VERSION ($GEOMETRY_NPM_VERSION)"
+    fi
 }
 
 geometry_plugin_register node
