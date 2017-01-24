9ee71d6257883568c10b3c7d1eb2eb625f7b3541
Okusanya Oluwadamilola <okusanya.damilola@gmail.com>
2017-01-24 21:44:08 +0000

    Removed unneccessary conditional and modified main conditional

--- plugins/node/plugin.zsh
+++ plugins/node/plugin.zsh
@@ -9,12 +9,10 @@ GEOMETRY_NODE_NPM_VERSION=$(prompt_geometry_colorize $GEOMETRY_COLOR_NODE_NPM_VE
 geometry_prompt_node_setup(){} 
 
 geometry_prompt_node_render(){	
-    if (( $+commands[node] == 0 )); then
+    if (( $+commands[node] != 0 )); then
         GEOMETRY_NODE_VERSION="$(node -v)"
         GEOMETRY_NPM_VERSION="$(npm --version)" 
         echo "$GEOMETRY_NODE_NPM_VERSION $GEOMETRY_NODE_VERSION ($GEOMETRY_NPM_VERSION)"
-    else
-        echo " "
     fi
 }
 
