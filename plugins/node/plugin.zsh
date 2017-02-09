a1dbfdb54438d85927c92c6d374c5ec006c8df42
Okusanya Oluwadamilola <okusanya.damilola@gmail.com>
2017-02-09 22:38:03 +0000

    Refactored the code. Much simpler/clearer that the last commit

--- plugins/node/plugin.zsh
+++ plugins/node/plugin.zsh
@@ -1,23 +1,31 @@
 # Color definitions
-GEOMETRY_COLOR_NODE_NPM_VERSION=${GEOMETRY_COLOR_NODE_NPM_VERSION:-black}
+GEOMETRY_COLOR_PACKAGER_VERSION=${GEOMETRY_COLOR_PACKAGER_VERSION:-black}
 
 # Symbol definitions
-GEOMETRY_SYMBOL_NODE_NPM_VERSION=${GEOMETRY_SYMBOL_NODE_NPM_VERSION:-"⬡"}
-GEOMETRY_NODE_NPM_VERSION=$(prompt_geometry_colorize $GEOMETRY_COLOR_NODE_NPM_VERSION $GEOMETRY_SYMBOL_NODE_NPM_VERSION) 
+GEOMETRY_SYMBOL_PACKAGER_VERSION=${GEOMETRY_SYMBOL_PACKAGER_VERSION:-"⬡"}
+GEOMETRY_NODE_PACKAGER_VERSION=$(prompt_geometry_colorize $GEOMETRY_COLOR_PACKAGER_VERSION $GEOMETRY_SYMBOL_PACKAGER_VERSION) 
+
+# Package manager
+GEOMETRY_NODE_DEFAULT_PACKAGE_MANAGER="npm"
 
 geometry_prompt_node_setup() {}
 
 geometry_prompt_node_render() {
-    local -a files
-    files=($(ls -a ${PWD} | egrep -e ".nvmrc|yarn.lock"))
-
-    if [[ ${#files[@]} -gt 0 ]]; then
-        if (( $+commands[node] )); then
-            GEOMETRY_NODE_VERSION="$(node -v 2> /dev/null)"
-            GEOMETRY_NPM_VERSION="$(npm --version 2> /dev/null)" 
-            echo "$GEOMETRY_NODE_NPM_VERSION $GEOMETRY_NODE_VERSION ($GEOMETRY_NPM_VERSION)"
-        fi
+    if [[ ! $+commands[node] ]]; then 
+        return
     fi
+
+    if [[ ! -f "$PWD/package.json" ]]; then
+        return
+    fi
+
+    if [[ $+commands[yarn] && -f "$PWD/yarn.lock" ]]; then
+        GEOMETRY_NODE_DEFAULT_PACKAGE_MANAGER="yarn"
+    fi 
+
+    GEOMETRY_PACKAGER_VERSION="$($GEOMETRY_NODE_DEFAULT_PACKAGE_MANAGER --version 2> /dev/null)" 
+    GEOMETRY_NODE_VERSION="$(node -v 2> /dev/null)"
+    echo "$GEOMETRY_NODE_PACKAGER_VERSION $GEOMETRY_NODE_VERSION ($GEOMETRY_PACKAGER_VERSION)"
 }
 
 geometry_plugin_register node
