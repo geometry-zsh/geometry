81dbb57a5730a9bd91dbbc0edccbf209864b98c6
Jonathan Dahan <hi@jonathan.is>
2017-02-10 00:39:50 -0500

    plugins/node: add * support to always render

--- plugins/node/plugin.zsh
+++ plugins/node/plugin.zsh
@@ -6,15 +6,16 @@ GEOMETRY_SYMBOL_NODE_NPM_VERSION=${GEOMETRY_SYMBOL_NODE_NPM_VERSION:-"⬡"}
 GEOMETRY_NODE_NPM_VERSION=$(prompt_geometry_colorize $GEOMETRY_COLOR_NODE_NPM_VERSION $GEOMETRY_SYMBOL_NODE_NPM_VERSION) 
 
 geometry_prompt_node_setup() {
-    starred=$1
+    _GEOMETRY_NODE_STARRED=$1
 }
 
 geometry_prompt_node_render() {
-    if (( $+commands[node] )); then
-        GEOMETRY_NODE_VERSION="$(node -v 2> /dev/null)"
-        GEOMETRY_NPM_VERSION="$(npm --version 2> /dev/null)" 
-        echo "$GEOMETRY_NODE_NPM_VERSION $GEOMETRY_NODE_VERSION ($GEOMETRY_NPM_VERSION)"
-    fi
+    (( $+commands[node] )) || return
+    { [[ $_GEOMETRY_NODE_STARRED ]] || test -f package.json } || return
+
+    GEOMETRY_NODE_VERSION="$(node -v 2> /dev/null)"
+    GEOMETRY_NPM_VERSION="$(npm --version 2> /dev/null)"
+    echo "$GEOMETRY_NODE_NPM_VERSION $GEOMETRY_NODE_VERSION ($GEOMETRY_NPM_VERSION)"
 }
 
 geometry_plugin_register node
