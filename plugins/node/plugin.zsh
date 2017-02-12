bdbe6500167d38059708914085a201639e2101fb
castellanprime <okusanya.damilola@gmail.com>
2017-02-12 08:21:06 -0600

    Added npm/yarn check for node plugin (#108)

--- plugins/node/plugin.zsh
+++ plugins/node/plugin.zsh
@@ -1,18 +1,31 @@
 # Color definitions
-GEOMETRY_COLOR_NODE_NPM_VERSION=${GEOMETRY_COLOR_NODE_NPM_VERSION:-black}
+GEOMETRY_COLOR_PACKAGER_VERSION=${GEOMETRY_COLOR_PACKAGER_VERSION:-black}
 
 # Symbol definitions
-GEOMETRY_SYMBOL_NODE_NPM_VERSION=${GEOMETRY_SYMBOL_NODE_NPM_VERSION:-"⬡"}
-GEOMETRY_NODE_NPM_VERSION=$(prompt_geometry_colorize $GEOMETRY_COLOR_NODE_NPM_VERSION $GEOMETRY_SYMBOL_NODE_NPM_VERSION) 
+GEOMETRY_SYMBOL_PACKAGER_VERSION=${GEOMETRY_SYMBOL_PACKAGER_VERSION:-"⬡"}
+GEOMETRY_NODE_PACKAGER_VERSION=$(prompt_geometry_colorize $GEOMETRY_COLOR_PACKAGER_VERSION $GEOMETRY_SYMBOL_PACKAGER_VERSION) 
 
 geometry_prompt_node_setup() {}
 
 geometry_prompt_node_render() {
-    if (( $+commands[node] )); then
-        GEOMETRY_NODE_VERSION="$(node -v 2> /dev/null)"
-        GEOMETRY_NPM_VERSION="$(npm --version 2> /dev/null)" 
-        echo "$GEOMETRY_NODE_NPM_VERSION $GEOMETRY_NODE_VERSION ($GEOMETRY_NPM_VERSION)"
+    if [[ ! $+commands[node] ]]; then 
+        return
     fi
+
+    if [[ ! -f "$PWD/package.json" ]]; then
+        return
+    fi
+
+    local GEOMETRY_NODE_DEFAULT_PACKAGE_MANAGER=npm
+
+    if [[ $+commands[yarn] && -f "$PWD/yarn.lock" ]]; then
+        GEOMETRY_NODE_DEFAULT_PACKAGE_MANAGER=yarn
+    fi 
+
+    GEOMETRY_PACKAGER_VERSION="$($GEOMETRY_NODE_DEFAULT_PACKAGE_MANAGER --version 2> /dev/null)" 
+    GEOMETRY_NODE_VERSION="$(node -v 2> /dev/null)"
+    echo "$GEOMETRY_NODE_PACKAGER_VERSION $GEOMETRY_NODE_VERSION ($GEOMETRY_NODE_DEFAULT_PACKAGE_MANAGER:$GEOMETRY_PACKAGER_VERSION)"
 }
 
 geometry_plugin_register node
+
