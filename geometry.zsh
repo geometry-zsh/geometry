a0d87ded009ae7c72f4cb567b08b01de923598ef
Darío Cavuotti <des@riseup.net>
2017-04-06 12:43:54 -0300

    Fix prompt_subst option set global

--- geometry.zsh
+++ geometry.zsh
@@ -40,7 +40,7 @@ GEOMETRY_PROMPT_SUFFIX=${GEOMETRY_PROMPT_SUFFIX:-''}
 prompt_geometry_set_cmd_title() {
   local COMMAND="${2}"
   local CURR_DIR="${PWD##*/}"
-  setopt localoptions prompt_subst
+  setopt localoptions no_prompt_subst
   print -n '\e]0;'
   print -Pn '$COMMAND @ $CURR_DIR'
   print -n '\a'
@@ -48,7 +48,6 @@ prompt_geometry_set_cmd_title() {
 
 # Prevent command showing on title after ending
 prompt_geometry_set_title() {
-  setopt localoptions prompt_subst
   print -n '\e]0;'
   print -Pn '%~'
   print -n '\a'
