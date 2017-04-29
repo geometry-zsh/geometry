181ec579e9775c3dcf291d3f5d307af8f5a14723
DC* <des@riseup.net>
2017-04-28 23:34:15 -0300

    Refactor async lib to use zsh-async

--- lib/async.zsh
+++ lib/async.zsh
@@ -1,49 +1,25 @@
-# Misc configurations
-typeset -g GEOMETRY_ASYNC_TMP_PATH=${GEOMETRY_ASYNC_TMP_PATH:-/tmp}
-typeset -g GEOMETRY_ASYNC_TMP_FILENAME_PREFIX='.geometry-rprompt-info-'
-typeset -g GEOMETRY_ASYNC_TMP_FULL_PATH="$GEOMETRY_ASYNC_TMP_PATH/$GEOMETRY_ASYNC_TMP_FILENAME_PREFIX"
-typeset -g GEOMETRY_ASYNC_PROC_ID=0
+source $GEOMETRY_ROOT/lib/zsh-async/async.zsh
 
-# Renders rprompt and puts result into `${GEOMETRY_ASYNC_TMP_FULL_PATH}$$`
-# then signals `USR1`
--geometry_async_function() {
-    echo "${(j/::/)$(prompt_geometry_render_rprompt)}" > ${GEOMETRY_ASYNC_TMP_FULL_PATH}$$
-    kill -s USR1 $$
-}
-
-# Launches `-geometry_async_function` keeping track of PID
-# and killing stalled processes if necessary
--geometry_async_precmd() {
+-geometry-async-redraw() {
     # kill any ongoing async_function
     if [[ "${GEOMETRY_ASYNC_PROC_ID}" != 0 ]]; then
         kill -- -$(ps -o pgid= $GEOMETRY_ASYNC_PROC_ID | grep -o '[0-9]*') &> /dev/null || :
     fi
 
-    -geometry_async_function &!
-    GEOMETRY_ASYNC_PROC_ID=$!
+    RPROMPT="${(j/::/)$(prompt_geometry_render_rprompt)}"
+
+    # redisplay
+    zle && zle reset-prompt
 }
 
-# Removes the async temp file when zsh exits
--geometry_async_zshexit() {
-    \rm -f "${GEOMETRY_ASYNC_TMP_FULL_PATH}$$"
+-geometry_async_job() {
+  async_job rprompt_worker -geometry-async-redraw
 }
 
-# geometry_async_setup adds zsh-hooks into precmd and zshexit, as well as a
-# signal trap on SIGUSR1, to support communication of asynchronous updates to
-# the running terminal.
+# Hooks precmd to run `-geometry_async_function` and hooks
+# `USR1` signal to comunicate with it
 geometry_async_setup() {
-    add-zsh-hook precmd '-geometry_async_precmd'
-    add-zsh-hook zshexit '-geometry_async_zshexit'
-
-    TRAPUSR1() {
-        # read from temp file
-        setopt localoptions no_prompt_subst
-        RPROMPT="$(<${GEOMETRY_ASYNC_TMP_FULL_PATH}$$)"
-
-        # reset proc number
-        GEOMETRY_ASYNC_PROC_ID=0
-
-        # redisplay
-        zle && zle reset-prompt
-    }
+    async
+    async_start_worker rprompt_worker -u -n
+    add-zsh-hook precmd -geometry_async_job
 }
