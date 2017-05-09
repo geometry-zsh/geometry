c0830d4f7e53d5d275f60488f8fd0aca5ccc4732
DC* <des@riseup.net>
2017-05-08 23:27:48 -0300

    zsh-async implementation (#88)
    * Refactor async lib to use zsh-async
    * Add manual install instructions

--- lib/async.zsh
+++ lib/async.zsh
@@ -1,49 +1,35 @@
-# Misc configurations
-typeset -g GEOMETRY_ASYNC_TMP_PATH=${GEOMETRY_ASYNC_TMP_PATH:-/tmp}
-typeset -g GEOMETRY_ASYNC_TMP_FILENAME_PREFIX='.geometry-rprompt-info-'
-typeset -g GEOMETRY_ASYNC_TMP_FULL_PATH="$GEOMETRY_ASYNC_TMP_PATH/$GEOMETRY_ASYNC_TMP_FILENAME_PREFIX"
-typeset -g GEOMETRY_ASYNC_PROC_ID=0
+source $GEOMETRY_ROOT/lib/zsh-async/async.zsh
 
-# Renders rprompt and puts result into `${GEOMETRY_ASYNC_TMP_FULL_PATH}$$`
-# then signals `USR1`
--geometry_async_function() {
-    echo "${(j/::/)$(prompt_geometry_render_rprompt)}" > ${GEOMETRY_ASYNC_TMP_FULL_PATH}$$
-    kill -s USR1 $$
+# Callback handler to properly render RPROMPT with calculated output
+-geometry-async-callback() {
+    local job="$1" code="$2" output="$3" exec_time="$4" stderr="$5"
+    RPROMPT="${(j/::/)output}"
+    zle && zle reset-prompt
 }
 
-# Launches `-geometry_async_function` keeping track of PID
-# and killing stalled processes if necessary
--geometry_async_precmd() {
-    # kill any ongoing async_function
-    if [[ "${GEOMETRY_ASYNC_PROC_ID}" != 0 ]]; then
-        kill -- -$(ps -o pgid= $GEOMETRY_ASYNC_PROC_ID | grep -o '[0-9]*') &> /dev/null || :
-    fi
-
-    -geometry_async_function &!
-    GEOMETRY_ASYNC_PROC_ID=$!
+# Wrapper to call rprompt renderer, needed to set up workers status
+-geometry-async-prompt() {
+    # In order to work with zsh-async we need to set workers in
+    # the proper directory.
+    cd -q $1 > /dev/null
+    prompt_geometry_render_rprompt
 }
 
-# Removes the async temp file when zsh exits
--geometry_async_zshexit() {
-    \rm -f "${GEOMETRY_ASYNC_TMP_FULL_PATH}$$"
+# Flushed currently running async jobs and queues a new one
+# See https://github.com/mafredri/zsh-async#async_flush_jobs-worker_name
+-geometry-async-job() {
+    async_flush_jobs geometry_async_worker 
+    async_job geometry_async_worker -geometry-async-prompt $PWD
 }
 
-# geometry_async_setup adds zsh-hooks into precmd and zshexit, as well as a
-# signal trap on SIGUSR1, to support communication of asynchronous updates to
-# the running terminal.
+# Initialize zsh-async and creates a worker
 geometry_async_setup() {
-    add-zsh-hook precmd '-geometry_async_precmd'
-    add-zsh-hook zshexit '-geometry_async_zshexit'
+    async_init
+    # See https://github.com/mafredri/zsh-async#async_start_worker-worker_name--u--n--p-pid
+    async_start_worker geometry_async_worker -u -n # unique, notify through SIGWINCH
+    async_register_callback geometry_async_worker -geometry-async-callback
 
-    TRAPUSR1() {
-        # read from temp file
-        setopt localoptions no_prompt_subst
-        RPROMPT="$(<${GEOMETRY_ASYNC_TMP_FULL_PATH}$$)"
-
-        # reset proc number
-        GEOMETRY_ASYNC_PROC_ID=0
-
-        # redisplay
-        zle && zle reset-prompt
-    }
+    # Submit a new job every precmd
+    add-zsh-hook precmd -geometry-async-job
 }
+
