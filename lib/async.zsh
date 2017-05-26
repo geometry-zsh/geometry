e8fe840278916346333411a5cb3532d0352482bf
DC* <des@riseup.net>
2017-05-26 10:32:48 -0300

    zsh-async worker missing env variables (#160)
    * zsh-async worker missing env variables
    * Explicitly stop workers

--- lib/async.zsh
+++ lib/async.zsh
@@ -5,6 +5,9 @@ source $GEOMETRY_ROOT/lib/zsh-async/async.zsh 2> /dev/null
     local job="$1" code="$2" output="$3" exec_time="$4" stderr="$5"
     RPROMPT="${(j/::/)output}"
     zle && zle reset-prompt
+
+    # Explicitely stop async worker
+    async_stop_worker geometry_async_worker
 }
 
 # Wrapper to call rprompt renderer, needed to set up workers status
@@ -18,6 +21,10 @@ source $GEOMETRY_ROOT/lib/zsh-async/async.zsh 2> /dev/null
 # Flushed currently running async jobs and queues a new one
 # See https://github.com/mafredri/zsh-async#async_flush_jobs-worker_name
 -geometry-async-job() {
+    # See https://github.com/mafredri/zsh-async#async_start_worker-worker_name--u--n--p-pid
+    async_start_worker geometry_async_worker -u -n # unique, notify through SIGWINCH
+    async_register_callback geometry_async_worker -geometry-async-callback
+
     async_flush_jobs geometry_async_worker 
     async_job geometry_async_worker -geometry-async-prompt $PWD
 }
@@ -33,9 +40,6 @@ geometry_async_setup() {
     fi
 
     async_init
-    # See https://github.com/mafredri/zsh-async#async_start_worker-worker_name--u--n--p-pid
-    async_start_worker geometry_async_worker -u -n # unique, notify through SIGWINCH
-    async_register_callback geometry_async_worker -geometry-async-callback
 
     # Submit a new job every precmd
     add-zsh-hook precmd -geometry-async-job
