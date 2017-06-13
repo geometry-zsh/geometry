0a28e1b1dd969c5cd34d14201f4c4aeee940197f
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-06-13 11:00:55 -0400

    Unify conda and virtualenv plugins

--- plugins/virtualenv/plugin.zsh
+++ plugins/virtualenv/plugin.zsh
@@ -1,15 +1,34 @@
 # Color definitions
-GEOMETRY_COLOR_VIRTUALENV=${GEOMETRY_COLOR_PROMPT:-green}
+DEFAULT_COLOR=${GEOMETRY_COLOR_PROMPT:-green}
+GEOMETRY_COLOR_VIRTUALENV=${GEOMETRY_COLOR_VIRTUALENV:-$DEFAULT_COLOR}
+GEOMETRY_COLOR_CONDA=${GEOMETRY_COLOR_CONDA:-$DEFAULT_COLOR}
+GEOMETRY_VIRTUALENV_CONDA_SEPARATOR=${GEOMETRY_VIRTUALENV_CONDA_SEPARATOR:-:}
+
 
 geometry_prompt_virtualenv_setup() {}
 
 geometry_prompt_virtualenv_check() {
-  test $VIRTUAL_ENV || return 1
+    [ -n "${VIRTUAL_ENV}" -o -n "${CONDA_PREFIX}" ]
 }
 
 geometry_prompt_virtualenv_render() {
-  local ref=$(basename $VIRTUAL_ENV)
-  echo "$(prompt_geometry_colorize $GEOMETRY_COLOR_VIRTUALENV "(${ref})")"
+
+    if [ -n "${VIRTUAL_ENV}" ]; then
+
+        virtualenv_ref=$(basename $VIRTUAL_ENV)
+        virtualenv_str="$(prompt_geometry_colorize $GEOMETRY_COLOR_VIRTUALENV ${virtualenv_ref})"
+
+        if [ -n "${CONDA_PREFIX}" ]; then
+            conda_ref="$(basename $CONDA_PREFIX)"
+            virtualenv_str="${virtualenv_str}${GEOMETRY_VIRTUALENV_CONDA_SEPARATOR}$(prompt_geometry_colorize $GEOMETRY_COLOR_CONDA ${conda_ref})"
+        fi
+
+        echo "$virtualenv_str"
+
+    elif [ -n "${CONDA_PREFIX}" ]; then
+        conda_ref="$(basename $CONDA_PREFIX)"
+        echo "$(prompt_geometry_colorize $GEOMETRY_COLOR_CONDA ${conda_ref})"
+    fi
 }
 
 # Self-register plugin
