0892d18b9a0c61253862be4014ff015a16f25778
Philipp Hanslovsky <Philipp.Hanslovsky@gmail.com>
2017-06-19 09:31:28 -0400

    Add conda support on virtualenv plugin (#166)

--- plugins/virtualenv/plugin.zsh
+++ plugins/virtualenv/plugin.zsh
@@ -1,15 +1,40 @@
 # Color definitions
-GEOMETRY_COLOR_VIRTUALENV=${GEOMETRY_COLOR_PROMPT:-green}
+DEFAULT_COLOR=${GEOMETRY_COLOR_PROMPT:-green}
+GEOMETRY_COLOR_VIRTUALENV=${GEOMETRY_COLOR_VIRTUALENV:-$DEFAULT_COLOR}
+GEOMETRY_COLOR_CONDA=${GEOMETRY_COLOR_CONDA:-$DEFAULT_COLOR}
+GEOMETRY_VIRTUALENV_CONDA_SEPARATOR=${GEOMETRY_VIRTUALENV_CONDA_SEPARATOR:-:}
+
 
 geometry_prompt_virtualenv_setup() {}
 
 geometry_prompt_virtualenv_check() {
-  test $VIRTUAL_ENV || return 1
+    [ -n "${VIRTUAL_ENV}" -o -n "${CONDA_PREFIX}" ]
 }
 
 geometry_prompt_virtualenv_render() {
-  local ref=$(basename $VIRTUAL_ENV)
-  echo "$(prompt_geometry_colorize $GEOMETRY_COLOR_VIRTUALENV "(${ref})")"
+
+    local environment_str=""
+
+    # Add virtualenv name if active
+    if [ -n "${VIRTUAL_ENV}" ]; then
+        local virtualenv_ref=$(basename $VIRTUAL_ENV)
+        environment_str="$(prompt_geometry_colorize $GEOMETRY_COLOR_VIRTUALENV ${virtualenv_ref})"
+    fi
+
+    # Add separator if both active
+    if [ -n "${VIRTUAL_ENV}" -a -n "${CONDA_PREFIX}" ]; then
+        environment_str="${environment_str}${GEOMETRY_VIRTUALENV_CONDA_SEPARATOR}"
+    fi
+
+    # Add conda environment name if active
+    if [ -n "${CONDA_PREFIX}" ]; then
+        local conda_ref="$(basename $CONDA_PREFIX)"
+        environment_str="${environment_str}$(prompt_geometry_colorize $GEOMETRY_COLOR_CONDA ${conda_ref})"
+    fi
+
+    # Print to stdout
+    echo "${environment_str}"
+
 }
 
 # Self-register plugin
