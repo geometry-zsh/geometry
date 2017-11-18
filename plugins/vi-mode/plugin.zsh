6b65cac443e4550b4979d00999fc3b68c12d584d
DC* <des@riseup.net>
2017-11-17 22:12:44 -0300

    Add comments

--- plugins/vi-mode/plugin.zsh
+++ plugins/vi-mode/plugin.zsh
@@ -1,10 +1,18 @@
-zle -N zle-keymap-select geometry_prompt_vi-mode_render
 
+# For some reason this line doesn't work inside a function, otherwise
+# it should be located in the _setup function.
+zle -N zle-keymap-select geometry_prompt_vi-mode_render
 geometry_prompt_vi-mode_setup() {}
+
 geometry_prompt_vi-mode_check() {}
+
 geometry_prompt_vi-mode_render() {
-    RPROMPT=${${RPROMPT/\[INSERT\] /}/\[NORMAL\] /}
-    local KEY=${${KEYMAP/main/INSERT}/vicmd/NORMAL}
-    RPROMPT="[$KEY] $RPROMPT"
-    zle && zle reset-prompt || echo -n $RPROMPT
+  # Removes previously state
+  RPROMPT=${${RPROMPT/\[INSERT\] /}/\[NORMAL\] /}
+  # swaps main/INSERT, vicmd/NORMAL
+  local KEY=${${KEYMAP/main/INSERT}/vicmd/NORMAL}
+  # Formats RPROMPT with mode + rest of prompt
+  RPROMPT="[$KEY] $RPROMPT"
+  # Resets prompt
+  zle && zle reset-prompt
 }
