4659cce687cd5e60776facd1df9e92d0a1b4c7b7
Jonathan Dahan <hi@jonathan.is>
2018-10-05 18:22:25 -0400

    remove _OK suffix

--- functions/geometry_status.zsh
+++ functions/geometry_status.zsh
@@ -2,13 +2,13 @@
 #
 # Display a symbol with error/success and root/non-root information
 
-: ${GEOMETRY_STATUS_COLOR_OK:=white}      # Color when everything is ok
+: ${GEOMETRY_STATUS_COLOR:=white}         # Color when everything is ok
 : ${GEOMETRY_STATUS_COLOR_ERROR:=magenta} # Color if there was an error
 : ${GEOMETRY_STATUS_COLOR_HASH:=false}    # Color indicates hostname
 
-: ${GEOMETRY_STATUS_SYMBOL_OK:=▲}         # Default symbol
+: ${GEOMETRY_STATUS_SYMBOL:=▲}            # Default symbol
 : ${GEOMETRY_STATUS_SYMBOL_ERROR:=△}      # Error symbol
-: ${GEOMETRY_SYMBOL_STATUS_ROOT_OK:=▲}    # Root symbol
+: ${GEOMETRY_SYMBOL_STATUS_ROOT:=▲}       # Root symbol
 : ${GEOMETRY_SYMBOL_STATUS_ROOT_ERROR:=△} # Root error symbol
 
 
@@ -30,23 +30,23 @@ _geometry_hash_color() {
 }
 
 # Combine color and symbols
-GEOMETRY_STATUS_OK=$(_geometry_colorize $GEOMETRY_STATUS_COLOR_OK $GEOMETRY_STATUS_SYMBOL_OK)
+GEOMETRY_STATUS=$(_geometry_colorize $GEOMETRY_STATUS_COLOR $GEOMETRY_STATUS_SYMBOL)
 GEOMETRY_STATUS_ERROR=$(_geometry_colorize $GEOMETRY_STATUS_COLOR_ERROR $GEOMETRY_STATUS_SYMBOL_ERROR)
-GEOMETRY_STATUS_ROOT_OK=$(_geometry_colorize $GEOMETRY_STATUS_COLOR_OK $GEOMETRY_STATUS_SYMBOL_ROOT_OK)
+GEOMETRY_STATUS_ROOT=$(_geometry_colorize $GEOMETRY_STATUS_COLOR $GEOMETRY_STATUS_SYMBOL_ROOT)
 GEOMETRY_STATUS_ROOT_ERROR=$(_geometry_colorize $GEOMETRY_STATUS_COLOR_ERROR $GEOMETRY_STATUS_SYMBOL_ROOT_ERROR)
 
 if $GEOMETRY_STATUS_SYMBOL_COLOR_HASH; then
-  GEOMETRY_STATUS_COLOR_OK=$(_geometry_hash_color $HOST)
-  GEOMETRY_STATUS_OK=$(_geometry_colorize $GEOMETRY_STATUS_COLOR_OK $GEOMETRY_STATUS_SYMBOL_OK)
+  GEOMETRY_STATUS_COLOR=$(_geometry_hash_color $HOST)
+  GEOMETRY_STATUS=$(_geometry_colorize $GEOMETRY_STATUS_COLOR $GEOMETRY_STATUS_SYMBOL)
 fi
 
 
 function geometry_status() {
-  local _status=$GEOMETRY_STATUS_OK
+  local _status=$GEOMETRY_STATUS
 
   if (( $+GEOMETRY_LAST_ERROR )); then
     if [[ $UID == 0 || $EUID == 0 ]]; then
-        _status=$GEOMETRY_STATUS_ROOT_OK
+        _status=$GEOMETRY_STATUS_ROOT
     fi
   else
     _status=$GEOMETRY_STATUS_ERROR
