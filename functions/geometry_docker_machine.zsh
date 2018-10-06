cfd8940626fd5d25471682827c1a86f78cbf8f8a
Jonathan Dahan <hi@jonathan.is>
2018-10-06 11:56:05 -0400

    port docker machine

---
+++ functions/geometry_docker_machine.zsh
@@ -0,0 +1,11 @@
+# Docker Machine
+#
+# Indicate the current docker machine name.
+
+: ${GEOMETRY_DOCKER_MACHINE_COLOR:=blue}
+: ${GEOMETRY_DOCKER_MACHINE_SYMBOL:="⚓"}
+
+function geometry_docker_machine {
+  test -n $DOCKER_MACHINE_NAME || return
+  echo -n "$(color $GEOMETRY_DOCKER_MACHINE_COLOR "(${GEOMETRY_DOCKER_MACHINE_SYMBOL} ${DOCKER_MACHINE_NAME})")"
+}
