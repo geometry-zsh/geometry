ccdf6799a6ad0d3bbf934e018616a083959fcf9e
Jonathan Dahan <hi@jonathan.is>
2018-10-06 11:53:26 -0400

    port jobs

---
+++ functions/geometry_jobs.zsh
@@ -0,0 +1,12 @@
+# Jobs
+#
+# Small plugin to display background jobs count.
+
+: ${GEOMETRY_JOBS_COLOR:=blue}
+: ${GEOMETRY_JOBS_SYMBOL:="⚙"}
+
+function geometry_jobs {
+  [[ $(print -P '%j') == "0" ]] || return
+  local jobs_prompt='%(1j.'$GEOMETRY_JOBS_SYMBOL' %j.)'
+  echo $(color $GEOMETRY_JOBS_COLOR $jobs_prompt)
+}
