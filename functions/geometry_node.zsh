4c1ad2f6d05d6fc381728d978c5889f3f50b489e
Jonathan Dahan <hi@jonathan.is>
2018-10-06 11:37:07 -0400

    Port node

---
+++ functions/geometry_node.zsh
@@ -0,0 +1,25 @@
+# Node
+#
+# Display node and npm/yarm version when in a node project context
+
+: ${GEOMETRY_NODE_COLOR:=green} # node color
+: ${GEOMETRY_NODE_SYMBOL:="⬡"}  # node symbol
+: ${GEOMETRY_NODE_PIN:=false}   # always display?
+
+(( $+commands[node] )) || (( $+commands[yarn] )) || return 1
+
+GEOMETRY_NODE=$(color $GEOMETRY_NODE_COLOR $GEOMETRY_NODE_SYMBOL)
+
+function geometry_node {
+    test $GEOMETRY_NODE_PIN || test -f package.json || test -f yarn.lock || return 1
+
+    local package_manager=npm
+
+    if [[ $+commands[yarn] && -f yarn.lock ]]; then
+        package_manager=yarn
+    fi
+
+    local packager_version="$($package_manager --version 2>/dev/null)"
+    local node_version="$(node -v 2>/dev/null)"
+    echo -n "$GEOEMETRY_NODE $node_version ($package_manager:$packager_version)"
+}
