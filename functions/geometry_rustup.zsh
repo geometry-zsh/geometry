2cd6619078824391feda0469f57eec677a01da24
Jonathan Dahan <hi@jonathan.is>
2018-10-14 12:18:03 -0400

    rustup: exit if no rustup is found on load

--- functions/geometry_rustup.zsh
+++ functions/geometry_rustup.zsh
@@ -2,13 +2,13 @@
 #
 # Display a symbol with the currently selected rustup toolchain
 
+(( $+commands[rustup] )) || return       # requires rustup
+
 : ${GEOMETRY_RUSTUP_STABLE_COLOR:=green} #
 : ${GEOMETRY_RUSTUP_BETA_COLOR:=yellow}  #
 : ${GEOMETRY_RUSTUP_NIGHTLY_COLOR:=red}  #
 : ${GEOMETRY_RUSTUP_SYMBOL:="⚙"}         # Symbol
 : ${GEOMETRY_RUSTUP_PIN:=false}          # Always display
-
-(( $+commands[rustup] )) || return
 
 function geometry_rustup {
     ( ! ${GEOMETRY_RUSTUP_PIN} ) && test ! `cargo locate-project --quiet` && return
