e7e7951a54d506db82f7839653144344dd31e50e
Jonathan Dahan <hi@jonathan.is>
2018-10-20 12:30:13 -0400

    lib/time: remove unnecessary globals

--- lib/time.zsh
+++ lib/time.zsh
@@ -5,52 +5,27 @@
 : ${GEOMETRY_COLOR_TIME_LONG:=red}
 : ${GEOMETRY_COLOR_NO_TIME:=red}
 
-typeset -g geometry_time_human
-typeset -g geometry_time_color
-
 # Format time in short format: 4s, 4h, 1d
 _geometry_time_short_format() {
-  local human=""
-  local color=""
-  local days=$1
-  local hours=$2
-  local minutes=$3
-  local seconds=$4
+  local days=$1 hours=$2 minutes=$3 seconds=$4
 
-  if (( days > 0 )); then
-    human="${days}d"
-    color=$GEOMETRY_COLOR_TIME_LONG
-  elif (( hours > 0 )); then
-    human="${hours}h"
-    : ${color:=$GEOMETRY_COLOR_TIME_NEUTRAL}
-  elif (( minutes > 0 )); then
-    human="${minutes}m"
-    : ${color:=$GEOMETRY_COLOR_TIME_SHORT}
-  else
-    human="${seconds}s"
-    : ${color:=$GEOMETRY_COLOR_TIME_SHORT}
-  fi
+  (( days > 0 )) && ansi $GEOMETRY_COLOR_TIME_LONG "${days}d" && return
+  (( hours > 0 )) && ansi $GEOMETRY_COLOR_TIME_NEUTRAL "${hours}h" && return
+  (( minutes > 0 )) && ansi $GEOMETRY_COLOR_TIME_SHORT "${minutes}m" && return
 
-  geometry_time_color=$color
-  geometry_time_human=$human
+  ansi $GEOMETRY_COLOR_TIME_SHORT "${seconds}s"
 }
 
 # Format time in long format: 1d4h33m51s, 33m51s
 _geometry_time_long_format() {
-  local human=""
-  local color=""
-  local days=$1
-  local hours=$2
-  local minutes=$3
-  local seconds=$4
+  local days=$1 hours=$2 minutes=$3 seconds=$4
+  local human="" color=""
 
-  (( days > 0 )) && human+="${days}d " && color=$GEOMETRY_COLOR_TIME_LONG
-  (( hours > 0 )) && human+="${hours}h " && : ${color:=$GEOMETRY_COLOR_TIME_NEUTRAL}
+  (( days > 0 ))    && human+="${days}d "    && color=$GEOMETRY_COLOR_TIME_LONG
+  (( hours > 0 ))   && human+="${hours}h "   && : ${color:=$GEOMETRY_COLOR_TIME_NEUTRAL}
   (( minutes > 0 )) && human+="${minutes}m "
-  human+="${seconds}s" && : ${color:=$GEOMETRY_COLOR_TIME_SHORT}
 
-  geometry_time_color=$color
-  geometry_time_human=$human
+  ansi ${color:=$GEOMETRY_COLOR_TIME_SHORT} "$human ${seconds}s"
 }
 
 # from https://github.com/sindresorhus/pretty-time-zsh
@@ -65,6 +40,4 @@ _geometry_seconds_to_human_time() {
 
   $long_format && _geometry_time_long_format $days $hours $minutes $seconds
   $long_format || _geometry_time_short_format $days $hours $minutes $seconds
-
-  echo "$(ansi $geometry_time_color $geometry_time_human)"
 }
