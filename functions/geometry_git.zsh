c298326ef86999267c4472199419b1fb9ed6c8b4
Jonathan Dahan <hi@jonathan.is>
2018-10-20 01:55:33 -0400

    remove unused -n in git _status

--- functions/geometry_git.zsh
+++ functions/geometry_git.zsh
@@ -24,7 +24,7 @@ _branch() {
 _status() {
   [[ -z "$(git status --porcelain --ignore-submodules HEAD)" ]] && [[ -z "$(git ls-files --others --modified --exclude-standard)" ]]
   _status=$($? && echo CLEAN || echo DIRTY)
-  echo -n $(ansi ${(e):-\$GEOMETRY_GIT_COLOR_${_status}} ${(e):-\$GEOMETRY_GIT_SYMBOL_${_status}})
+  echo $(ansi ${(e):-\$GEOMETRY_GIT_COLOR_${_status}} ${(e):-\$GEOMETRY_GIT_SYMBOL_${_status}})
 }
 
 _rebase() {
