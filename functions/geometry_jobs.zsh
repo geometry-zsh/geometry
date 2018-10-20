259f78397fedae766075a4a452ed2d5f80907bcb
Jonathan Dahan <hi@jonathan.is>
2018-10-20 13:14:59 -0400

    geometry_jobs real one-liner

--- functions/geometry_jobs.zsh
+++ functions/geometry_jobs.zsh
@@ -1,5 +1,3 @@
-# geometry_jobs - show background jobs count
+# geometry_jobs - show background jobs count `⚙ 1`
 
-function geometry_jobs {
-  ansi ${GEOMETRY_JOBS_COLOR:=blue} '%(1j.'${GEOMETRY_JOBS_SYMBOL:="⚙"}' %j.)'
-}
+function geometry_jobs { ansi ${GEOMETRY_JOBS_COLOR:=blue} '%(1j.'${GEOMETRY_JOBS_SYMBOL:="⚙"}' %j.)' }
