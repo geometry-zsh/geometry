73b030ead698847be46583fb7fd0ccfea93fc6bd
Jonathan Dahan <hi@jonathan.is>
2018-10-20 13:13:55 -0400

    make geometry_jobs a one-liner

--- functions/geometry_jobs.zsh
+++ functions/geometry_jobs.zsh
@@ -1,10 +1,5 @@
 # geometry_jobs - show background jobs count
 
 function geometry_jobs {
-  : ${GEOMETRY_JOBS_COLOR:=blue}
-  : ${GEOMETRY_JOBS_SYMBOL:="⚙"}
-
-  [[ $(print -P '%j') == "0" ]] || return
-  local jobs_prompt='%(1j.'$GEOMETRY_JOBS_SYMBOL' %j.)'
-  ansi $GEOMETRY_JOBS_COLOR $jobs_prompt
+  ansi ${GEOMETRY_JOBS_COLOR:=blue} '%(1j.'${GEOMETRY_JOBS_SYMBOL:="⚙"}' %j.)'
 }
