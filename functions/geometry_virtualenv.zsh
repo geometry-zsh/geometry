f173ff64a1fe90db288b7aa3c5dc9243d42c0251
Jonathan Dahan <hi@jonathan.is>
2018-10-22 01:08:38 -0400

    geometry_virtualenv: use :t instead of basename in param expansion

--- functions/geometry_virtualenv.zsh
+++ functions/geometry_virtualenv.zsh
@@ -1,7 +1,7 @@
 # geometry_virtualenv - show the current `virtualenv` or `conda` environment
 
 function geometry_virtualenv {
-    test  -n "${VIRTUAL_ENV}${CONDA_PREFIX}" || return
+    [[ -n "${VIRTUAL_ENV}${CONDA_PREFIX}" ]] || return
 
     DEFAULT_COLOR=${GEOMETRY_COLOR_PROMPT:-green}
 
@@ -12,21 +12,16 @@ function geometry_virtualenv {
     local environment_str=""
 
     # Add virtualenv name if active
-    if [[ -n "${VIRTUAL_ENV}" ]]; then
-        local virtualenv_ref=$(basename $VIRTUAL_ENV)
-        environment_str="$(ansi $GEOMETRY_VIRTUALENV_COLOR ${virtualenv_ref})"
-    fi
+    [[ -n "${VIRTUAL_ENV}" ]] \
+      && environment_str="$(ansi $GEOMETRY_VIRTUALENV_COLOR ${VIRTUAL_ENV:t})"
 
     # Add separator if both active
-    if [[ -n "${VIRTUAL_ENV}" && -n "${CONDA_PREFIX}" ]]; then
-        environment_str="${environment_str}${GEOMETRY_VIRTUALENV_CONDA_SEPARATOR}"
-    fi
+    [[ -n "${VIRTUAL_ENV}" && -n "${CONDA_PREFIX}" ]] \
+      && environment_str+="${GEOMETRY_VIRTUALENV_CONDA_SEPARATOR}"
 
     # Add conda environment name if active
-    if [[ -n "${CONDA_PREFIX}" ]]; then
-        local conda_ref="$(basename $CONDA_PREFIX)"
-        environment_str="${environment_str}$(ansi $GEOMETRY_VIRUALENV_CONDA_COLOR ${conda_ref})"
-    fi
+    [[ -n "${CONDA_PREFIX}" ]] \
+      && environment_str+="$(ansi $GEOMETRY_VIRUALENV_CONDA_COLOR ${CONDA_PREFIX:t})"
 
     echo -n "${environment_str}"
 }
