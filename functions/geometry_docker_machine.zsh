0865bcb92261a4ef6f492fa5ef636991aa77456e
Jonathan Dahan <hi@jonathan.is>
2018-10-25 14:38:25 -0400

    migrate docker_machine
    - breaking changes: GEOMETRY_X_DOCKER_MACHINE => GEOMETRY_DOCKER_MACHINE_X

---
+++ functions/geometry_docker_machine.zsh
@@ -0,0 +1,10 @@
+# geometry_docker_machine - show the current docker machine name
+
+geometry_docker_machine() {
+  [[ -n $DOCKER_MACHINE_NAME ]] || return
+
+  : ${GEOMETRY_DOCKER_MACHINE_COLOR:=blue}
+  : ${GEOMETRY_DOCKER_MACHINE_SYMBOL:="⚓"}
+
+  ansi $GEOMETRY_DOCKER_MACHINE_COLOR "(${GEOMETRY_DOCKER_MACHINE_SYMBOL} ${DOCKER_MACHINE_NAME})"
+}
