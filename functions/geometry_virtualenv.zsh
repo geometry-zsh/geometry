d37aa710039ad84d16e95e8fe97f485c8f08e27f
Jonathan Dahan <hi@jonathan.is>
2018-10-25 14:37:27 -0400

    migrate virtualenv
    - breaking changes: GEOMETRY_X_VIRTUALENV => GEOMETRY_VIRTUALENV_X

---
+++ functions/geometry_virtualenv.zsh
@@ -0,0 +1,27 @@
+# geometry_virtualenv - show the current `virtualenv` or `conda` environment
+
+geometry_virtualenv() {
+    [[ -n "${VIRTUAL_ENV}${CONDA_PREFIX}" ]] || return
+
+    DEFAULT_COLOR=${GEOMETRY_COLOR_PROMPT:-green}
+
+    : ${GEOMETRY_VIRTUALENV_COLOR:=$DEFAULT_COLOR}
+    : ${GEOMETRY_VIRTUALENV_CONDA_COLOR:=$DEFAULT_COLOR}
+    : ${GEOMETRY_VIRTUALENV_CONDA_SEPARATOR:=:}
+
+    local environment_str=""
+
+    # Add virtualenv name if active
+    [[ -n "${VIRTUAL_ENV}" ]] \
+      && environment_str="$(ansi $GEOMETRY_VIRTUALENV_COLOR ${VIRTUAL_ENV:t})"
+
+    # Add separator if both active
+    [[ -n "${VIRTUAL_ENV}" && -n "${CONDA_PREFIX}" ]] \
+      && environment_str+="${GEOMETRY_VIRTUALENV_CONDA_SEPARATOR}"
+
+    # Add conda environment name if active
+    [[ -n "${CONDA_PREFIX}" ]] \
+      && environment_str+="$(ansi $GEOMETRY_VIRUALENV_CONDA_COLOR ${CONDA_PREFIX:t})"
+
+    echo "${environment_str}"
+}
