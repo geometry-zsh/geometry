1e28b723c1ed1dd3c0697de3328e14adec784def
Jonathan Dahan <hi@jonathan.is>
2018-10-25 14:35:30 -0400

    migrate hostname
    - removed GEOMETRY_PLUGIN_HOSTNAME_PREFIX
    - removed GEOMETRY_PLUGIN_HOSTNAME_SUFFIX
    
    - added GEOMETRY_HOSTNAME_SEPARATOR
    - added GEOMETRY_HOSTNAME_HIDE_ON to hide on specified host (default localhost)

---
+++ functions/geometry_hostname.zsh
@@ -0,0 +1,7 @@
+# geometry_hostname - show user@hostname
+
+geometry_hostname() {
+  local _host=${HOST:-$HOSTNAME}
+  [[ "$_host" = "${GEOMETRY_HOSTNAME_HIDE_ON:=localhost}" ]] && return
+  echo -n "${USER}${GEOMETRY_HOSTNAME_SEPARATOR:=@}${_host}"
+}
