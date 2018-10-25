e2d26cd50521424580b0a4fd98b8312276300e01
Jonathan Dahan <hi@jonathan.is>
2018-10-25 14:38:48 -0400

    migrate exec_time
    - breaking change: PROMPT_GEOMETRY_COMMAND_EXEC_TIME => GEOMETRY_EXEC_TIME_PATIENCE

---
+++ functions/geometry_exec_time.zsh
@@ -0,0 +1,34 @@
+# geometry_exec_time - show the elapsed time for long running commands
+#
+# See ![long_running](../screenshots/long_running.png) for an example
+
+zmodload zsh/datetime || return # required for `$EPOCHSECONDS`
+
+typeset -g _geometry_command_timestamp
+typeset -g _geometry_command_exec_time
+
+# Stores (into _geometry_command_exec_time) the exec time of the last command if set threshold was exceeded
+_geometry_check_command_exec_time() {
+  : ${GEOMETRY_EXEC_TIME_PATIENCE:=5} # How many seconds to wait before showing
+
+  integer elapsed
+  # Default value for exec_time is an empty string (ie, it won't be rendered),
+  # if we don't clear this up it may be rendered each time
+  _geometry_command_exec_time=
+
+  # Check if elapsed time is above the configured threshold
+  (( elapsed = EPOCHSECONDS - ${_geometry_command_timestamp:-$EPOCHSECONDS} ))
+  (( elapsed > $GEOMETRY_EXEC_TIME_PATIENCE )) && _geometry_command_exec_time="$(_geometry_seconds_to_human_time $elapsed)"
+
+  # Clear timestamp after we're done calculating exec_time
+  _geometry_command_timestamp=
+}
+
+_geometry_set_command_timestamp() { _geometry_command_timestamp=$EPOCHSECONDS }
+
+# Begin to track the EPOCHSECONDS since this command is executed
+add-zsh-hook preexec _geometry_set_command_timestamp
+# Check if we need to display execution time
+add-zsh-hook precmd _geometry_check_command_exec_time
+
+function geometry_exec_time { echo "$_geometry_command_exec_time" }
