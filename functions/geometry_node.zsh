a9ecc3dc35be231b437c0d0a6742aa57ab16d5f2
Jonathan Dahan <hi@jonathan.is>
2018-10-25 14:38:00 -0400

    migrate node
    - breaking changes: GEOMETRY_X_NODE => GEOMETRY_NODE_X
    - added GEOMETRY_NODE_PIN for pinning

---
+++ functions/geometry_node.zsh
@@ -0,0 +1,17 @@
+# geometry_node - show node and npm/yarm version when in a node project context
+
+(( $+commands[node] )) || return
+
+geometry_node() {
+    (( ${GEOMETRY_NODE_PIN:=false} )) || [[ -f package.json ]] || [[ -f yarn.lock ]] || return
+
+    GEOMETRY_NODE=$(ansi ${GEOMETRY_NODE_COLOR:=green} ${GEOMETRY_NODE_SYMBOL="⬡"})
+
+    local package_manager=npm
+
+    (( $+commands[yarn] )) && [[ -f yarn.lock ]] && package_manager=yarn
+
+    local packager_version="$($package_manager --version 2>/dev/null)"
+    local node_version="$(node -v 2>/dev/null)"
+    echo -n "$GEOMETRY_NODE $node_version ($package_manager:$packager_version)"
+}
