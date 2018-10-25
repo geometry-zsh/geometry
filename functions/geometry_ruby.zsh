af92aa777d912adb841fef02dd6fa7b3fdfa9dc6
Jonathan Dahan <hi@jonathan.is>
2018-10-25 14:37:47 -0400

    migrate ruby
    - breaking changes: GEOMETRY_X_RUBY => GEOMETRY_RUBY_X

---
+++ functions/geometry_ruby.zsh
@@ -0,0 +1,23 @@
+# geometry_ruby - display the current ruby version, rvm version, and gemset
+
+(( $+commands[ruby] )) || return
+
+geometry_ruby() {
+  GEOMETRY_RUBY=$(ansi ${GEOMETRY_RUBY_COLOR:=white} ${GEOMETRY_RUBY_SYMBOL:="◆"})
+
+  [[ $(ruby -v) =~ 'ruby ([0-9a-zA-Z.]+)' ]]
+  local ruby_version=$match[1]
+
+  (( $+commands[rvm] )) && {
+    [[ $(rvm -v) =~ 'rvm ([0-9a-zA-Z.]+)' ]]
+    local rvm_version=$match[1]
+
+
+    (( ${GEOMETRY_RUBY_RVM_SHOW_GEMSET:=true} )) && {
+      [[ $(rvm current) =~ 'ruby-[0-9.]+@?(.*)' ]]
+      local gemset=${match[1]:-"default"}
+    }
+  }
+
+  echo -n "${(j: :):-$GEOMETRY_RUBY $ruby_version $rvm_version $gemset}"
+}
