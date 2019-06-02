fae975dff2947ccde9eb82e2c90f6586c0b635dc
Jonathan Dahan <hi@jonathan.is>
2019-06-02 17:22:41 -0400

    Check for jobs before printing, fixes #253 for realz

--- functions/geometry_jobs.zsh
+++ functions/geometry_jobs.zsh
@@ -1,3 +1,3 @@
 # geometry_jobs - show background jobs count `⚙ 1`
 
-geometry_jobs() { ansi ${GEOMETRY_JOBS_COLOR:=blue} '%(1j.'${GEOMETRY_JOBS_SYMBOL:="⚙"}' %j.)'; }
+geometry_jobs() {  [[ 0 -ne "$(jobs | wc -l)" ]] && ansi ${GEOMETRY_JOBS_COLOR:=blue} '%(1j.'${GEOMETRY_JOBS_SYMBOL:="⚙"}' %j.)'; }
