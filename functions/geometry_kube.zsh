651ea4f959ff9698b5406e7fb10111e69eeb1f2b
Oleg Tarassov <oleg.tarassov@guavus.com>
2019-08-28 08:47:56 -0400

    Added Improvements and PR Recommendations
    - Group Variable declaration
    - Append kube info to array
    - Added ZSH parameter expansion

--- functions/geometry_kube.zsh
+++ functions/geometry_kube.zsh
@@ -5,26 +5,36 @@
 geometry_kube() {
   ( ${GEOMETRY_KUBE_PIN:=false} ) || [[ ! -z "${KUBECONFIG}" ]] || return
 
-  local geometry_kube=$(ansi ${GEOMETRY_KUBE_COLOR:=blue} ${GEOMETRY_KUBE_SYMBOL:="⎈"})
-  local geometry_kube_separator=${GEOMETRY_KUBE_SEPARATOR:="|"}
+  # Variable declaration
+  local -a geometry_kube
+  local geometry_kube_separator
+  local kube_context
+  local kube_namespace
+
+  geometry_kube+=$(ansi ${GEOMETRY_KUBE_COLOR:=blue} ${GEOMETRY_KUBE_SYMBOL:="⎈"})
+  geometry_kube_separator=${GEOMETRY_KUBE_SEPARATOR:="|"}
+
+  kube_context="$(kubectl config current-context 2> /dev/null)"
+  kube_namespace="$(kubectl config view -o "jsonpath={.contexts[?(@.name==\"${kube_context}\")].context.namespace}" 2> /dev/null)"
+  kube_namespace=${kube_namespace:=default}
 
   if ( ${GEOMETRY_KUBE_VERSION:=true} ); then
     [[ $(kubectl version --client --short) =~ 'Client Version: ([0-9a-zA-Z.]+)' ]]
     geometry_kube+=($match[1])
   fi
 
-  local geometry_context="$(kubectl config current-context 2> /dev/null)"
-  local geometry_namespace="$(kubectl config view -o "jsonpath={.contexts[?(@.name==\"$context\")].context.namespace}" 2> /dev/null)"
-
   if [[ ! -z "${GEOMETRY_KUBE_CONTEXT_COLOR}" ]]; then
-    geometry_context="$(ansi ${GEOMETRY_KUBE_CONTEXT_COLOR:=white} ${geometry_context})"
+    geometry_kube+=("$(ansi ${GEOMETRY_KUBE_CONTEXT_COLOR} ${kube_context})")
+  else
+    geometry_kube+=("${kube_context}")
   fi
 
   if [[ ! -z "${GEOMETRY_KUBE_NAMESPACE_COLOR}" ]]; then
-    geometry_namespace="$(ansi ${GEOMETRY_KUBE_NAMESPACE_COLOR:=white} ${geometry_namespace:=default})"
+    geometry_kube+=("$(ansi ${GEOMETRY_KUBE_NAMESPACE_COLOR} ${kube_namespace})")
   else
-    geometry_namespace="${geometry_namespace:=default}"
+    geometry_kube+=("${kube_namespace}")
   fi
 
-  echo -n "${geometry_kube_separator}${geometry_kube} ${geometry_context}:${geometry_namespace}${geometry_kube_separator}"
+  echo -n "${geometry_kube_separator}${(pj.:.)geometry_kube}${geometry_kube_separator}"
+
 }
