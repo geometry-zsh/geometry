2773303ab724ae6d4c5b6d4d8d6e09b0d19ad626
Jonathan Dahan <hi@jonathan.is>
2019-08-29 11:29:43 +0300

    simplify kubectl string processing

--- functions/geometry_kube.zsh
+++ functions/geometry_kube.zsh
@@ -3,38 +3,30 @@
 (( $+commands[kubectl] )) || return
 
 geometry_kube() {
-  ( ${GEOMETRY_KUBE_PIN:=false} ) || [[ ! -z "${KUBECONFIG}" ]] || return
+  ( ${GEOMETRY_KUBE_PIN:=false} ) || [[ -n "$KUBECONFIG" ]] || return
+  : ${GEOMETRY_KUBE_SEPARATOR:="|"}
 
   # Variable declaration
   local -a geometry_kube
-  local geometry_kube_separator
+  local kube_symbol
   local kube_context
   local kube_namespace
+  local kube_version
 
-  geometry_kube+=$(ansi ${GEOMETRY_KUBE_COLOR:=blue} ${GEOMETRY_KUBE_SYMBOL:="⎈"})
-  geometry_kube_separator=${GEOMETRY_KUBE_SEPARATOR:="|"}
+  kube_symbol=$(ansi ${GEOMETRY_KUBE_COLOR:=blue} ${GEOMETRY_KUBE_SYMBOL:="⎈"})
 
   kube_context="$(kubectl config current-context 2> /dev/null)"
-  kube_namespace="$(kubectl config view -o "jsonpath={.contexts[?(@.name==\"${kube_context}\")].context.namespace}" 2> /dev/null)"
-  kube_namespace=${kube_namespace:=default}
+  kube_context=$(ansi ${GEOMETRY_KUBE_CONTEXT_COLOR:=default} ${kube_context})
+
+  kube_namespace=$(kubectl config view -o "jsonpath={.contexts[?(@.name==\"${kube_context}\")].context.namespace}" 2> /dev/null)
+  kube_namespace=$(ansi ${GEOMETRY_KUBE_NAMESPACE_COLOR:=default} ${kube_namespace:=default})
 
   if ( ${GEOMETRY_KUBE_VERSION:=true} ); then
     [[ $(kubectl version --client --short) =~ 'Client Version: ([0-9a-zA-Z.]+)' ]]
-    geometry_kube+=($match[1])
+    kube_version=($match[1])
   fi
 
-  if [[ ! -z "${GEOMETRY_KUBE_CONTEXT_COLOR}" ]]; then
-    geometry_kube+=("$(ansi ${GEOMETRY_KUBE_CONTEXT_COLOR} ${kube_context})")
-  else
-    geometry_kube+=("${kube_context}")
-  fi
+  geometry_kube=($kube_symbol $kube_context $kube_namespace $kube_version)
 
-  if [[ ! -z "${GEOMETRY_KUBE_NAMESPACE_COLOR}" ]]; then
-    geometry_kube+=("$(ansi ${GEOMETRY_KUBE_NAMESPACE_COLOR} ${kube_namespace})")
-  else
-    geometry_kube+=("${kube_namespace}")
-  fi
-
-  echo -n "${geometry_kube_separator}${(pj.:.)geometry_kube}${geometry_kube_separator}"
-
+  echo -n ${(pj.$GEOMETRY_KUBE_SEPARATOR.)geometry_kube}
 }
