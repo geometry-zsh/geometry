7e39b3612a1ffc9f874a5663ab7e508a86de7c60
Alexandre Beaulieu <alex@segfault.me>
2019-10-18 13:50:14 -0400

    feat: Added ZLE widget handling for vi-mode indicator.

--- functions/geometry_vi.zsh
+++ functions/geometry_vi.zsh
@@ -1,25 +1,77 @@
-# A geometry plugin to display the ZLE vi-mode state.
+# geometry_vi - A geometry plugin to display the ZLE vi-mode state.
 
-update_rprompt() {
-    RPROMPT=${${RPROMPT/\[NORMAL\]/}/\[INSERT\]/}
-    RPROMPT="[$1]$RPROMPT"
+geometry_vi() {
+    # Required for RPROMPT to render properly after submitting a command.
+    geometry::vi-get-mode
 }
 
-update_prompt() {
-    PROMPT=${${PROMPT/\[NORMAL\]/}/\[INSERT\]/}
-    PROMPT="[$1]$PROMPT"
+# Re-render the prompt.
+function geometry::vi-update-prompt() {
+    imode=$(geometry::vi-insert-mode)
+    nmode=$(geometry::vi-normal-mode)
+    if [[ $KEYMAP == vicmd ]]; then
+	PROMPT=${PROMPT//$imode/$nmode}
+	RPROMPT=${RPROMPT//$imode/$nmode}
+    else
+	PROMPT=${PROMPT//$nmode/$imode}
+	RPROMPT=${RPROMPT//$nmode/$imode}
+    fi
 }
 
-function zle-keymap-select {
-    keymap=$([[ $KEYMAP == vicmd ]] && echo "NORMAL" || echo "INSERT")
-    (( ${GEOMETRY_RPROMPT[(i)geometry_vi]} <= ${#GEOMETRY_RPROMPT} )) && update_rprompt $keymap
-    (( ${GEOMETRY_PROMPT[(i)geometry_vi]} <= ${#GEOMETRY_PROMPT} )) && update_prompt $keymap
+# Render normal mode.
+function geometry::vi-normal-mode() {
+    ansi ${GEOMETRY_VI_NORMAL_COLOR:-white} ${GEOMETRY_VI_NORMAL_MODE:-"<NORMAL>"}
+}
+
+# Render insert mode.
+function geometry::vi-insert-mode() {
+    ansi ${GEOMETRY_VI_INSERT_COLOR:-yellow} ${GEOMETRY_VI_INSERT_MODE:-"<INSERT>"}
+}
+
+# Return the currently active mode.
+function geometry::vi-get-mode() {
+    imode=$(geometry::vi-insert-mode)
+    nmode=$(geometry::vi-normal-mode)
+    [[ $KEYMAP == vicmd ]] && echo $nmode || echo $imode
+}
+
+# ZLE widget to trigger prompt updates.
+function geometry::vi-draw-mode {
+    keymap=$(geometry::vi-get-mode)
+    geometry::vi-update-prompt
     zle reset-prompt
 }
 
-zle -N zle-keymap-select
 
-geometry_vi() {
-    # Empty render function, these are handled by zle-keymap-select
-}
+# Initialization: Bind the widgets required to perform proper prompt updates.
+# Adapted from https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/zsh-syntax-highlighting.zsh#L295-L359
+local prefix="geometry_vi"
+for w (zle-keymap-select zle-line-pre-redraw zle-line-init zle-line-finish); do
+    case ${widgets[$w]:-""} in
+	# Already Bound to geometry: Do nothing.
+	user:geometry-vi-draw) ;;
 
+	# Existing user widget: Chain a call to it.
+	user:*) zle -N $prefix-$w ${widgets[$w]#*:}
+		# Dynamically create a function to call the previous widget.
+		eval "geometry::${(q)prefix}-${(q)w}() { geometry::vi-draw-mode; builtin zle ${(q)prefix}-${(q)w} -- \"\$@\" }"
+		zle -N $w geometry::$prefix-$w;;
+
+	# Completion widgets: Ignore.
+	completion:*) ;;
+
+	# Builtin widget: Chain a call to ".widget".
+	# Eval is required to get a closure (
+	builtin) eval "geometry::${(q)prefix}-${(q)w}() { geometry::vi-draw-mode; builtin zle .${(q)w} -- \"\$@\" }"
+		 zle -N $cur_widget _zsh_highlight_widget_$prefix-$cur_widget;;
+
+	# Unbound widget: Bind directly.
+	*) 
+	    if [[ $w == zle-* ]] && (( ! ${+widgets[$w]} )); then
+		zle -N $w geometry::vi-draw-mode
+	    else
+		# Default: unhandled case.
+		print -r -- >&2 "geometry::geometry_vi: unhandled ZLE widget ${(qq)w}"
+	    fi
+    esac
+done
